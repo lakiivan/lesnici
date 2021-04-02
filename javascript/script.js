da9525289f9e0339604a92b45ee8be63c7888382
lakiivan <ivanzlazic@gmial.com>
2021-04-02 19:32:23 +0200

    =orderi button ispisuje current date ispod

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,24 @@
-function activateOrder() {
-  console.log("Hello World");
-  
+function displayDate() {
+	document.getElementById('data').innerHTML = Date();
+
 }
+
+//retrieve json file
+function retrieveOreders() {
+	var myData = JSON.parse(data);
+	alert(myData[0].firstName);
+	alert(myData[0].lastName);
+	
+}
+
+
+
+(function (global) {
+
+	var test = {};
+
+	console.log("We are about to start.");
+
+	global.test = test;
+
+})(window);
